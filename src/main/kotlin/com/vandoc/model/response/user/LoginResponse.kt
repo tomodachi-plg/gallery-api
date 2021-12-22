928c9915e686425295340c4721996d1ef197deee
Ichvandi <ichvandi.maulana@stockbit.com>
2021-12-22 22:29:00 +0700

    Add LoginResponse model

---
+++ src/main/kotlin/com/vandoc/model/response/user/LoginResponse.kt
@@ -0,0 +1,17 @@
+package com.vandoc.model.response.user
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginResponse(
+    val user: UserResponse,
+    val token: String
+) {
+    data class UserResponse(
+        @SerializedName("fullname")
+        val fullName: String,
+
+        val username: String,
+
+        val email: String
+    )
+}
