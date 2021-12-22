148c292f03c8c51349a6d048277deb8a55a61f2a
Ichvandi <ichvandi.maulana@stockbit.com>
2021-12-22 22:24:12 +0700

    Inject mongo db

---
+++ src/main/kotlin/com/vandoc/di/MainModule.kt
@@ -0,0 +1,22 @@
+package com.vandoc.di
+
+import org.koin.dsl.module
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
+
+val mainModule = module {
+
+    single {
+        val host = System.getenv("MONGO_HOST")
+        val username = System.getenv("MONGO_USERNAME")
+        val password = System.getenv("MONGO_PASSWORD")
+        val cluster = System.getenv("MONGO_CLUSTER")
+        val dbname = System.getenv("MONGO_DB_NAME")
+        val query = "?retryWrites=true&w=majority"
+        val connectionString = "$host://$username:$password@$cluster/$dbname$query"
+
+        val client = KMongo.createClient(connectionString).coroutine
+        client.getDatabase(dbname)
+    }
+
+}
