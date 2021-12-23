d3f27fd53a1367abfc4ba6c22b46b9639ebc6a67
Ichvandi <ichvandi.maulana@stockbit.com>
2021-12-23 21:48:07 +0700

    Add server error ext function

--- src/main/kotlin/com/vandoc/utils/Routing.kt
+++ src/main/kotlin/com/vandoc/utils/Routing.kt
@@ -5,6 +5,7 @@ import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
 
+// 2XX
 suspend fun <T> ApplicationCall.ok(message: String, data: T?) {
     response.status(HttpStatusCode.OK)
     respond(
@@ -12,8 +13,17 @@ suspend fun <T> ApplicationCall.ok(message: String, data: T?) {
     )
 }
 
+// 4XX
 suspend fun ApplicationCall.badRequest(message: String) {
     response.status(HttpStatusCode.BadRequest)
+    respond(
+        Response(message, null)
+    )
+}
+
+// 5XX
+suspend fun ApplicationCall.serverError(message: String) {
+    response.status(HttpStatusCode.InternalServerError)
     respond(
         Response(message, null)
     )
