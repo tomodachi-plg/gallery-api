2d880c60ce606722c29d7f5fca95939c06f91ff7
Ichvandi <ichvandi.maulana@stockbit.com>
2021-12-23 21:49:06 +0700

    Implement register route

--- src/main/kotlin/com/vandoc/routes/Authentication.kt
+++ src/main/kotlin/com/vandoc/routes/Authentication.kt
@@ -2,10 +2,10 @@ package com.vandoc.routes
 
 import com.vandoc.model.db.User
 import com.vandoc.model.request.LoginRequest
+import com.vandoc.model.request.RegisterRequest
 import com.vandoc.model.response.user.LoginResponse
-import com.vandoc.utils.badRequest
-import com.vandoc.utils.generateJwt
-import com.vandoc.utils.ok
+import com.vandoc.model.response.user.RegisterResponse
+import com.vandoc.utils.*
 import io.ktor.application.*
 import io.ktor.request.*
 import io.ktor.routing.*
@@ -52,6 +52,93 @@ fun Routing.registerAuthenticationRoutes() {
             message = "Login success",
             data = loginResponse
         )
+    }
+
+    post("/register") {
+        val user = call.receive<RegisterRequest>()
+
+        if (user.fullName.isNullOrBlank()) {
+            call.badRequest("fullname can't be empty!")
+            return@post
+        }
+
+        if (user.username.isNullOrBlank()) {
+            call.badRequest("username can't be empty!")
+            return@post
+        }
+
+        if (user.username.length < 6) {
+            call.badRequest("username can't less than 6 characters!")
+            return@post
+        }
+
+        val userCollection = database.getCollection<User>("users")
+        val isUsernameExist = userCollection.findOne(User::username eq user.username) != null
+
+        if (isUsernameExist) {
+            call.badRequest("username already exists!")
+            return@post
+        }
+
+        if (user.email.isNullOrBlank()) {
+            call.badRequest("email can't be empty!")
+            return@post
+        }
+
+        if (!isValidEmail(user.email)) {
+            call.badRequest("email is malformed!")
+            return@post
+        }
+
+        val isEmailExist = userCollection.findOne(User::email eq user.email) != null
+
+        if (isEmailExist) {
+            call.badRequest("email already exists!")
+            return@post
+        }
+
+        if (user.password.isNullOrBlank()) {
+            call.badRequest("password can't be empty!")
+            return@post
+        }
+
+        if (user.password.length < 8) {
+            call.badRequest("password can't less than 8 characters!")
+            return@post
+        }
+
+        val hashedPassword = BCrypt.hashpw(user.password, BCrypt.gensalt())
+        val newUser = User(
+            fullName = user.fullName,
+            username = user.username,
+            email = user.email,
+            password = hashedPassword
+        )
+
+        userCollection.insertOne(newUser)
+
+        val isSuccess = userCollection.findOne(User::_id eq newUser._id) != null
+
+        if (!isSuccess) {
+            call.serverError("Failed to register user, please try again later.")
+            return@post
+        }
+
+        val token = generateJwt(newUser.username, newUser.email)
+        val registerResponse = RegisterResponse(
+            user = RegisterResponse.UserResponse(
+                fullName = newUser.fullName,
+                username = newUser.username,
+                email = newUser.email
+            ),
+            token = token
+        )
+
+        call.ok(
+            message = "Register success",
+            data = registerResponse
+        )
+
     }
 
 }
