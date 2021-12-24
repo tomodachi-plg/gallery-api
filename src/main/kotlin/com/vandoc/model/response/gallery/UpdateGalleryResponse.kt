43f0362f649449ab0ba1e4a28da9977e655eaad8
Ichvandi <ichvandi.maulana@stockbit.com>
2021-12-24 21:17:36 +0700

    Add UpdateGalleryResponse

---
+++ src/main/kotlin/com/vandoc/model/response/gallery/UpdateGalleryResponse.kt
@@ -0,0 +1,11 @@
+package com.vandoc.model.response.gallery
+
+import com.google.gson.annotations.SerializedName
+
+data class UpdateGalleryResponse(
+    @SerializedName("gallery_id")
+    val galleryId: String,
+    val caption: String,
+    @SerializedName("images_url")
+    val imagesUrl: List<String>
+)
