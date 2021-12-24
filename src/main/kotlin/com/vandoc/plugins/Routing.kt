9068a1d9fcc7e92e81d6456f4a1f07f0cbc3d0e8
Ichvandi <ichvandi.maulana@stockbit.com>
2021-12-24 20:31:42 +0700

    Handle unauthorized request

--- src/main/kotlin/com/vandoc/plugins/Routing.kt
+++ src/main/kotlin/com/vandoc/plugins/Routing.kt
@@ -1,13 +1,24 @@
 package com.vandoc.plugins
 
 import com.vandoc.routes.registerAuthenticationRoutes
+import com.vandoc.routes.registerGalleryRoutes
+import com.vandoc.utils.unauthorized
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
 import io.ktor.routing.*
 
 fun Application.configureRouting() {
 
+    install(StatusPages) {
+        status(HttpStatusCode.Unauthorized) {
+            call.unauthorized("Bad token or token already expired!")
+        }
+    }
+
     routing {
         registerAuthenticationRoutes()
+        registerGalleryRoutes()
     }
 
 }
