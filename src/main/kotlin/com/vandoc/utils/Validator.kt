5240721df69766bd064d528ca128755d8c979f04
Ichvandi <ichvandi.maulana@stockbit.com>
2021-12-25 16:02:25 +0700

    Add helper for checking is file image or video

--- src/main/kotlin/com/vandoc/utils/Validator.kt
+++ src/main/kotlin/com/vandoc/utils/Validator.kt
@@ -1,6 +1,25 @@
 package com.vandoc.utils
 
-fun isValidEmail(email: String): Boolean {
+import java.io.FileInputStream
+import java.net.URLConnection
+
+fun String.isValidEmail(): Boolean {
     val emailRegex = "^[A-Za-z](.*)([@])(.+)(\\.)(.+)"
-    return emailRegex.toRegex().matches(email)
+    return emailRegex.toRegex().matches(this)
+}
+
+fun String.isImageFile(): Boolean {
+    return this.startsWith("image")
+}
+
+fun FileInputStream.isImageFile(): Boolean {
+    return URLConnection.guessContentTypeFromStream(this).startsWith("image")
+}
+
+fun String.isVideoFile(): Boolean {
+    return this.startsWith("video")
+}
+
+fun FileInputStream.isVideoFile(): Boolean {
+    return URLConnection.guessContentTypeFromStream(this).startsWith("video")
 }
