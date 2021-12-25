73f4259942a240cdfcc50c3e370253cc93d8e012
Ichvandi <ichvandi.maulana@stockbit.com>
2021-12-25 19:43:42 +0700

    Move set db logger level to di module

--- src/main/kotlin/com/vandoc/Application.kt
+++ src/main/kotlin/com/vandoc/Application.kt
@@ -1,15 +1,10 @@
 package com.vandoc
 
-import ch.qos.logback.classic.Level
-import ch.qos.logback.classic.LoggerContext
 import com.vandoc.plugins.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import org.slf4j.LoggerFactory
 
 fun main() {
-    (LoggerFactory.getILoggerFactory() as LoggerContext).getLogger("org.mongodb.driver").level = Level.ERROR
-
     val port = System.getenv("PORT")?.toInt()
     embeddedServer(Netty, port = port ?: 8081) {
         configureInjection()
--- src/main/kotlin/com/vandoc/di/MainModule.kt
+++ src/main/kotlin/com/vandoc/di/MainModule.kt
@@ -1,10 +1,13 @@
 package com.vandoc.di
 
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.LoggerContext
 import com.google.firebase.cloud.StorageClient
 import com.vandoc.utils.UploadManager
 import org.koin.dsl.module
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
+import org.slf4j.LoggerFactory
 
 val mainModule = module {
 
@@ -16,6 +19,8 @@ val mainModule = module {
         val dbname = System.getenv("MONGO_DB_NAME")
         val query = "?retryWrites=true&w=majority"
         val connectionString = "$host://$username:$password@$cluster/$dbname$query"
+
+        (LoggerFactory.getILoggerFactory() as LoggerContext).getLogger("org.mongodb.driver").level = Level.ERROR
 
         val client = KMongo.createClient(connectionString).coroutine
         client.getDatabase(dbname)
